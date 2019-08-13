4a7e680c16c45669e845d3b4d378d0aaeb3010da
Bogdan Balias <bogdan.balias@teonica.com>
2019-08-13 18:15:48 +0300

    add profile matchesChallenges and matchesTournaments

--- src/pages/MatchesChallenges/MatchesChallenges.styles.ts
+++ src/pages/MatchesChallenges/MatchesChallenges.styles.ts
@@ -1,4 +1,5 @@
 import { StyleSheet } from 'react-native'
+import { colorShadow, colorOrangeText, colorBlack, colorTextGray, colorBlueStart } from '../../variables';
 
 export default StyleSheet.create({
 	container: {
@@ -8,16 +9,100 @@ export default StyleSheet.create({
 	iconCancel: {
 		marginRight: 15
 	},
-	headerRightText: {
-		fontFamily: "montserrat-semibold",
-		fontSize: 12,
-		color: "white"
+	calendar: {
+		shadowColor: colorBlack,
+		shadowOffset: {
+			width: 0,
+			height: 4
+		},
+		shadowOpacity: 0.2,
+		shadowRadius: 5,
+		elevation: 4,
+		marginBottom: 20,
 	},
-	form: {
+	wrapItem: {
+		flexDirection: "row",
+		padding: 15,
+		marginBottom: 10,
+		marginRight: 30,
+		backgroundColor: "white",
+		borderTopRightRadius: 15,
+		borderBottomRightRadius: 15,
+		shadowColor: colorShadow,
+		shadowOffset: {
+			width: 0,
+			height: 4
+		},
+		shadowOpacity: 0.2,
+		shadowRadius: 5,
+		elevation: 4
+	},
+	itemLeft: {
+		flex: 1
+	},
+	coinWrap: {
+		flexDirection: "row"
+	},
+	coinText: {
+		color: colorOrangeText,
+		fontSize: 12,
+		fontFamily: "montserrat-semibold",
+		marginLeft: 5,
+	},
+	nameText: {
+		color: colorBlack,
+		fontSize: 14,
+		fontFamily: "montserrat-semibold",
+		marginVertical: 10,
+	},
+	smallText: {
+		color: colorTextGray,
+		fontSize: 12,
+		fontFamily: "montserrat-medium",
+	},
+	itemRight: {
 		flex: 1,
+		flexDirection: "row",
 		justifyContent: "space-between"
 	},
-	btnNext: {
-		marginBottom: 25
+	wrapScores: {
+		justifyContent: 'center'
+	},
+	scoreText: {
+		fontSize: 12,
+		fontFamily: "montserrat-semibold",
+		color: colorBlack,
+		marginRight: 5,
+	},
+	wrapVS: {
+		width: 24,
+		height: 24,
+		borderRadius: 12,
+		backgroundColor: colorBlueStart,
+		borderWidth: 1,
+		borderColor: 'white',
+		alignItems: 'center',
+		justifyContent: 'center',
+		position: 'absolute',
+		top: 30
+	},
+	textVS: {
+		fontSize: 10,
+		fontFamily: "montserrat-semibold",
+		color: 'white',
+	},
+	wrapAvatars: {
+		justifyContent: "space-between",
+		position: 'relative',
+		alignItems: 'center',
+		height: 80
+	},
+	noDataText: {
+		fontSize: 14,
+		fontFamily: "montserrat-medium",
+		color: colorTextGray,
+		width: '100%',
+		textAlign: 'center',
+		marginTop: 15,
 	}
 })
